482f0ad479fa3d27dff40ca4190ec07df9af605e
Kushal Gulaskar <kushal.gulaskar@outlook.com>
2018-12-15 19:41:57 +0530

    jjjjjjjjjjjjjj

--- ConsoleApp1/Abstract Class/Program.cs
+++ ConsoleApp1/Abstract Class/Program.cs
@@ -15,7 +15,7 @@ namespace Interfacelesson
             FullTimeEmp fte = new FullTimeEmp()
             {
                 ID = 102,
-                FirstName = "Kushal...",
+                FirstName = "Kushal...Email",
                 LastName = "Email Notification Test.1..",
                 AnnualSalary = 60000
             };
